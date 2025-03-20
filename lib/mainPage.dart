65fcb4070f547280b69a7ae06a23de6bb099d740
Mailon Bido <mailonbido@gmail.com>
2025-03-19 22:32:57 -0300

    Upgrade to SDK 3.0

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
-  final String title;
-  MainPage({Key key, this.title}) : super(key: key);
+  final String? title;
+  MainPage({Key? key, this.title}) : super(key: key);
 
   @override
   _MainPageState createState() => _MainPageState();
@@ -14,9 +14,9 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     AppBar appBar = AppBar(
-      title: Text(widget.title),
+      title: Text(widget.title!),
       actions: <Widget>[
-        FlatButton(
+        TextButton(
           child: Text(
             fill ? 'Unfill' : 'Fill',
             style: TextStyle(color: Theme.of(context).secondaryHeaderColor),
